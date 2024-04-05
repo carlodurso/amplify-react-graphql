ce9ea7aaa0c923dcb46b8b1b7c0a72e3c06b5920
Carlo D'Urso <carlodurso@gmail.com>
2024-04-05 15:51:47 +0100

    latest changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql725a6e2e": {
       "AppClientID": "string",
